47482b5946b84a5af7ef3ba688201aa62da71515
gento <41165204+dscpsyl@users.noreply.github.com>
2024-09-21 23:29:42 -0700

    feat:Add landing page

---
+++ logettracker/tracker/static/tracker/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach((card, _) => {
+        const randomDuration = Math.random() * 25 + 5; // between 5 and 30 seconds
+        card.style.animationDuration = `${randomDuration}s`;
+    });
+});
