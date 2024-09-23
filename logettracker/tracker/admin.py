fd2e11e7040e9be9c9adee08f005c20479e2891f
Restyled.io <commits@restyled.io>
2024-09-23 06:57:15 +0000

    Restyled by reorder-python-imports

--- logettracker/tracker/admin.py
+++ logettracker/tracker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import LoGetCards, LoGetUsers
+from .models import LoGetCards
+from .models import LoGetUsers
 
 admin.site.register(LoGetUsers)
 admin.site.register(LoGetCards)
